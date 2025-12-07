81df257481d62a0e68f8849d634480bfe541c928
Bolaji Alabi <bolaji.o.alabi@icloud.com>
2025-12-07 18:08:25 -0500

    adding Wire.h to main and changing sensor data to c file

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "sensorData.h"
+#include "Wire.h"
 
 // put function declarations here:
 
@@ -7,15 +8,19 @@ void setup() {
   // put your setup code here, to run once:
   //Wire.begin();
   Serial.begin(9600);
-  SetupSensor();
-
   DDRC = 0;
   DDRC |= (1 << PC7);
   PORTC &= ~(1<<PC7);
+  Serial.println("Hello There");
+  Wire.begin();
+  SetupSensor();
+  
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   UpdateSensor();
-  Serial.print(GetMagnitude());
+  Serial.println(GetMagnitude());
 }
